c4b5f95e59047ff5821c0966e90b17cb0535270f
Veronika <vkotsubiak@gmail.com>
2022-11-05 14:31:37 +0200

    1

---
+++ main.py
@@ -0,0 +1,11 @@
+try:
+    nm = int(input('1 Число '))
+    nm1 = int(input('2 Число '))
+    nm2 = int(input('3 Число '))
+    nm3 = int(input('4 Число '))
+    nm4 = int(input('5 Число '))
+    snm = (nm+nm1+nm2+nm3+nm4)/5
+except:
+    print()
+
+print(snm)
